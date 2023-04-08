c9170a33afcbf27a7d84a41ba9756f0ccc164311
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-04-08 20:49:44 +0200

    Implementing getCorsiIscritti method

--- src/main/java/com/example/gestorecorsi/db/CorsoDAO.java
+++ src/main/java/com/example/gestorecorsi/db/CorsoDAO.java
@@ -7,7 +7,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CorsoDAO {
 
@@ -15,17 +17,19 @@ public class CorsoDAO {
 
         List<Corso> corsi = new ArrayList<>();
 
+        String sql = "SELECT * FROM corso WHERE pd = ?";
+
         try {
             Connection conn = DBConnect.getConnection();
-
-            String sql = "SELECT * FROM corso WHERE pd = ?";
 
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setInt(1, pd);
             ResultSet rs = ps.executeQuery();
 
             while(rs.next()){
-                Corso corso = new Corso(rs.getString("codins"), rs.getInt("crediti"), rs.getString("nome"), rs.getInt("pd"));
+                Corso corso = new Corso(rs.getString("codins"), rs.getInt("crediti"),
+                        rs.getString("nome"), rs.getInt("pd"));
+
                 corsi.add(corso);
             }
 
@@ -36,6 +40,45 @@ public class CorsoDAO {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    public Map<Corso, Integer> getCorsiIscritti(int periodo) {
+
+        String sql = "SELECT c.codins, c.crediti, c.nome, c.pd, COUNT(*) AS n "
+                + "FROM corso c, iscrizione i "
+                + "WHERE c.codins = i.codins and c.pd = ? "
+                + "GROUP BY c.codins, c.crediti, c.nome, c.pd";
+
+        Map<Corso, Integer> result = new HashMap<>();
+
+        try {
+
+            Connection conn = DBConnect.getConnection();
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, periodo);
+
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+
+                Corso corso = new Corso(rs.getString("codins"),
+                        rs.getInt("crediti"), rs.getString("nome"),
+                        rs.getInt("pd"));
+
+                result.put(corso, rs.getInt("n"));
+            }
+
+            st.close();
+            rs.close();
+            conn.close();
+            return result;
+
+        } catch(SQLException e) {
+            System.out.println("Error in Corso DAO");
+            e.printStackTrace();
+            return null;
+
         }
     }
 }
